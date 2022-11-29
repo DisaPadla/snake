7c3459fdc9606541b0f2591bee8170a31a987380
ReasonX7 <reasonx7@gmail.com>
2022-11-29 06:32:00 +0200

    base layout

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import serve from 'rollup-plugin-serve'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+  },
+  plugins: [
+    serve({
+      open: true,
+      contentBase: ['dist', 'static'],
+    }),
+  ],
+};
